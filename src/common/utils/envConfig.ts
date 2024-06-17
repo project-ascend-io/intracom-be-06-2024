6aae4053f9b231bbb3047087c0ffb0838e58ada7
bobbygrdn <bobbygrdn777@gmail.com>
2024-06-17 11:04:25 -0700

    Revert "feat: updates the envConfig to include our mongodb connection string"
    This reverts commit 98ea1021d65a8178aa1933828bd1128fc8291b4a.

--- src/common/utils/envConfig.ts
+++ src/common/utils/envConfig.ts
@@ -13,5 +13,5 @@ export const env = cleanEnv(process.env, {
   CORS_ORIGIN: str({ devDefault: testOnly('http://localhost:3000') }),
   COMMON_RATE_LIMIT_MAX_REQUESTS: num({ devDefault: testOnly(1000) }),
   COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
-  MONGODB_CONNECTION_STRING: 'mongodb://root:root@localhost:27017/',
+  MONGODB_CONNECTION_STRING: str(),
 });
