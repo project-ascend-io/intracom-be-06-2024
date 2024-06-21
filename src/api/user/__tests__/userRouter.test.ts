d17eeebc97c2734815b5785f885d772fb7c44f0b
Oscar Estrada <16450128+oestrada1001@users.noreply.github.com>
2024-06-21 09:39:35 -0700

    fix(user tests): implemented before each hooks and mocking users

--- src/api/user/__tests__/userRouter.test.ts
+++ src/api/user/__tests__/userRouter.test.ts
@@ -1,11 +1,10 @@
 import { StatusCodes } from 'http-status-codes';
 import mongoose from 'mongoose';
 import request from 'supertest';
-import { afterEach, describe, expect, it, Mock, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, Mock, TaskContext, TestContext, vi } from 'vitest';
 
 import { User } from '@/api/user/userSchema';
 import { userService } from '@/api/user/userService';
-// import { UserModel as User } from '@/api/user/userModel';
 import { ResponseStatus, ServiceResponse } from '@/common/models/serviceResponse';
 import { app } from '@/server';
 
@@ -18,30 +17,37 @@ vi.mock('../userService', () => ({
   },
 }));
 
+interface TaskContext {
+  userList: User[];
+}
+
+type UserEndpointTestContext = TestContext & TaskContext;
+
 describe('User API Endpoints', () => {
+  beforeEach(async (context: UserEndpointTestContext) => {
+    const userList: User[] = [];
+    for (let i: number = 0; i < 10; i++) {
+      const objectId = new mongoose.mongo.ObjectId();
+      const prefix = `johndoe+${i}`;
+      userList.push({
+        id: objectId.toString(),
+        email: prefix + '@gmail.com',
+        username: prefix,
+        password: 'testing123!',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+    context.userList = userList;
+  });
   afterEach(() => {
     vi.clearAllMocks();
   });
-  // Mock Users
-
   describe('GET /users', () => {
-    it('should return a list of users', async () => {
-      const userList: User[] = [];
-      for (let i: number = 0; i < 10; i++) {
-        const objectId = new mongoose.mongo.ObjectId();
-        const prefix = `johndoe+${i}`;
-        userList.push({
-          id: objectId.toString(),
-          email: prefix + '@gmail.com',
-          username: prefix,
-          password: 'testing123!',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        });
-      }
+    it('should return a list of users', async ({ userList }: UserEndpointTestContext) => {
       const responseMock = new ServiceResponse<User[]>(ResponseStatus.Success, 'Users found', userList, StatusCodes.OK);
-
       (userService.findAll as Mock).mockReturnValue(responseMock);
+
       // Act
       const response = await request(app).get('/users');
       const responseBody: ServiceResponse<User[]> = response.body;
@@ -50,68 +56,83 @@ describe('User API Endpoints', () => {
       expect(response.statusCode).toEqual(StatusCodes.OK);
       expect(responseBody.success).toBeTruthy();
       expect(responseBody.message).toContain('Users found');
-      // expect(responseBody.responseObject.length).toEqual(users.length);
-      // responseBody.responseObject.forEach((user, index) => compareUsers(users[index] as User, user));
+      expect(responseBody.responseObject.length).toEqual(userList.length);
+      responseBody.responseObject.forEach((user, index) => compareUsers(userList[index] as User, user));
     });
   });
 
-  // describe('GET /users/:id', () => {
-  //   it('should return a user for a valid ID', async () => {
-  //     // Arrange
-  //     const testId = 1;
-  //     const expectedUser = users.find((user) => user.id === testId) as User;
-  //
-  //     // Act
-  //     const response = await request(app).get(`/users/${testId}`);
-  //     const responseBody: ServiceResponse<User> = response.body;
-  //
-  //     // Assert
-  //     expect(response.statusCode).toEqual(StatusCodes.OK);
-  //     expect(responseBody.success).toBeTruthy();
-  //     expect(responseBody.message).toContain('User found');
-  //     if (!expectedUser) throw new Error('Invalid test data: expectedUser is undefined');
-  //     compareUsers(expectedUser, responseBody.responseObject);
-  //   });
-  //
-  //   it('should return a not found error for non-existent ID', async () => {
-  //     // Arrange
-  //     const objectId = new mongoose.mongo.ObjectId();
-  //
-  //     // Act
-  //     const response = await request(app).get(`/users/${objectId.toString()}`);
-  //     const responseBody: ServiceResponse = response.body;
-  //
-  //     // Assert
-  //     expect(response.statusCode).toEqual(StatusCodes.NOT_FOUND);
-  //     expect(responseBody.success).toBeFalsy();
-  //     expect(responseBody.message).toContain('User not found');
-  //     expect(responseBody.responseObject).toBeNull();
-  //   });
-  //
-  //   it('should return a bad request for invalid ID format', async () => {
-  //     // Act
-  //     const invalidInput = 1;
-  //     const response = await request(app).get(`/users/${invalidInput}`);
-  //     const responseBody: ServiceResponse = response.body;
-  //
-  //     // Assert
-  //     expect(response.statusCode).toEqual(StatusCodes.BAD_REQUEST);
-  //     expect(responseBody.success).toBeFalsy();
-  //     expect(responseBody.message).toContain('Invalid input');
-  //     expect(responseBody.responseObject).toBeNull();
-  //   });
-  // });
+  describe('GET /users/:id', () => {
+    it('should return a user for a valid ID', async ({ userList }) => {
+      // Arrange
+      const testId = userList['1'].id;
+      const expectedUser = userList.find((user: User) => user.id === testId) as User;
+
+      const responseMock = new ServiceResponse<User>(
+        ResponseStatus.Success,
+        'User found',
+        expectedUser,
+        StatusCodes.OK
+      );
+      (userService.findById as Mock).mockReturnValue(responseMock);
+
+      // Act
+      const response = await request(app).get(`/users/${testId}`);
+      const responseBody: ServiceResponse<User> = response.body;
+
+      // Assert
+      expect(response.statusCode).toEqual(StatusCodes.OK);
+      expect(responseBody.success).toBeTruthy();
+      expect(responseBody.message).toContain('User found');
+      if (!expectedUser) throw new Error('Invalid test data: expectedUser is undefined');
+      compareUsers(expectedUser, responseBody.responseObject);
+    });
+
+    it('should return a not found error for non-existent ID', async () => {
+      // Arrange
+      const objectId = new mongoose.mongo.ObjectId();
+
+      const responseMock = new ServiceResponse<null>(
+        ResponseStatus.Failed,
+        'User not found',
+        null,
+        StatusCodes.NOT_FOUND
+      );
+
+      (userService.findById as Mock).mockReturnValue(responseMock);
+      // Act
+      const response = await request(app).get(`/users/${objectId.toString()}`);
+      const responseBody: ServiceResponse = response.body;
+
+      // Assert
+      expect(response.statusCode).toEqual(StatusCodes.NOT_FOUND);
+      expect(responseBody.success).toBeFalsy();
+      expect(responseBody.message).toContain('User not found');
+      expect(responseBody.responseObject).toBeNull();
+    });
+
+    it('should return a bad request for invalid ID format', async () => {
+      // Act
+      const invalidInput = 1;
+      const response = await request(app).get(`/users/${invalidInput}`);
+      const responseBody: ServiceResponse = response.body;
+
+      // Assert
+      expect(response.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+      expect(responseBody.success).toBeFalsy();
+      expect(responseBody.message).toContain('Invalid input');
+      expect(responseBody.responseObject).toBeNull();
+    });
+  });
 });
 
-// function compareUsers(mockUser: User, responseUser: User) {
-//   if (!mockUser || !responseUser) {
-//     throw new Error('Invalid test data: mockUser or responseUser is undefined');
-//   }
-//
-//   expect(responseUser.id).toEqual(mockUser.id);
-//   expect(responseUser.name).toEqual(mockUser.name);
-//   expect(responseUser.email).toEqual(mockUser.email);
-//   expect(responseUser.age).toEqual(mockUser.age);
-//   expect(new Date(responseUser.createdAt)).toEqual(mockUser.createdAt);
-//   expect(new Date(responseUser.updatedAt)).toEqual(mockUser.updatedAt);
-// }
+function compareUsers(mockUser: User, responseUser: User) {
+  if (!mockUser || !responseUser) {
+    throw new Error('Invalid test data: mockUser or responseUser is undefined');
+  }
+
+  expect(responseUser.id).toEqual(mockUser.id);
+  expect(responseUser.username).toEqual(mockUser.username);
+  expect(responseUser.email).toEqual(mockUser.email);
+  expect(new Date(responseUser.createdAt)).toEqual(mockUser.createdAt);
+  expect(new Date(responseUser.updatedAt)).toEqual(mockUser.updatedAt);
+}
--- src/api/user/__tests__/userService.test.ts
+++ src/api/user/__tests__/userService.test.ts
@@ -78,51 +78,51 @@ describe('userService', () => {
     });
   });
 
-  // describe('findById', () => {
-  //   it('returns a user for a valid ID', async () => {
-  //     // Arrange
-  //     const testId = 1;
-  //     const mockUser = mockUsers.find((user) => user.id === testId);
-  //     (userRepository.findByIdAsync as Mock).mockReturnValue(mockUser);
-  //
-  //     // Act
-  //     const result = await userService.findById(testId);
-  //
-  //     // Assert
-  //     expect(result.statusCode).toEqual(StatusCodes.OK);
-  //     expect(result.success).toBeTruthy();
-  //     expect(result.message).toContain('User found');
-  //     expect(result.responseObject).toEqual(mockUser);
-  //   });
-  //
-  //   it('handles errors for findByIdAsync', async () => {
-  //     // Arrange
-  //     const testId = 1;
-  //     (userRepository.findByIdAsync as Mock).mockRejectedValue(new Error('Database error'));
-  //
-  //     // Act
-  //     const result = await userService.findById(testId);
-  //
-  //     // Assert
-  //     expect(result.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
-  //     expect(result.success).toBeFalsy();
-  //     expect(result.message).toContain(`Error finding user with id ${testId}`);
-  //     expect(result.responseObject).toBeNull();
-  //   });
-  //
-  //   it('returns a not found error for non-existent ID', async () => {
-  //     // Arrange
-  //     const testId = 1;
-  //     (userRepository.findByIdAsync as Mock).mockReturnValue(null);
-  //
-  //     // Act
-  //     const result = await userService.findById(testId);
-  //
-  //     // Assert
-  //     expect(result.statusCode).toEqual(StatusCodes.NOT_FOUND);
-  //     expect(result.success).toBeFalsy();
-  //     expect(result.message).toContain('User not found');
-  //     expect(result.responseObject).toBeNull();
-  //   });
-  // });
+  describe('findById', () => {
+    it('returns a user for a valid ID', async () => {
+      // Arrange
+      const testId = '1';
+      const mockUser = mockUsers.find((user) => user.id === testId);
+      (userRepository.findByIdAsync as Mock).mockReturnValue(mockUser);
+
+      // Act
+      const result = await userService.findById(testId);
+
+      // Assert
+      expect(result.statusCode).toEqual(StatusCodes.OK);
+      expect(result.success).toBeTruthy();
+      expect(result.message).toContain('User found');
+      expect(result.responseObject).toEqual(mockUser);
+    });
+
+    it('handles errors for findByIdAsync', async () => {
+      // Arrange
+      const testId = '1';
+      (userRepository.findByIdAsync as Mock).mockRejectedValue(new Error('Database error'));
+
+      // Act
+      const result = await userService.findById(testId);
+
+      // Assert
+      expect(result.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+      expect(result.success).toBeFalsy();
+      expect(result.message).toContain(`Error finding user with id ${testId}`);
+      expect(result.responseObject).toBeNull();
+    });
+
+    it('returns a not found error for non-existent ID', async () => {
+      // Arrange
+      const testId = '1';
+      (userRepository.findByIdAsync as Mock).mockReturnValue(null);
+
+      // Act
+      const result = await userService.findById(testId);
+
+      // Assert
+      expect(result.statusCode).toEqual(StatusCodes.NOT_FOUND);
+      expect(result.success).toBeFalsy();
+      expect(result.message).toContain('User not found');
+      expect(result.responseObject).toBeNull();
+    });
+  });
 });
