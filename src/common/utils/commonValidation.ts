e9176ab9835bfc27f8bbfbefb92d39f338c080e3
Oscar Estrada <16450128+oestrada1001@users.noreply.github.com>
2024-06-27 15:59:09 -0700

    fix(error message): improved validation error message

--- src/common/utils/commonValidation.ts
+++ src/common/utils/commonValidation.ts
@@ -7,6 +7,6 @@ export const commonValidations = {
     .refine((data) => ObjectId.isValid(data), 'id must be a valid ObjectId')
     .transform((data) => new ObjectId(data)),
   email: z.string().email({ message: 'Please enter a valid email address.' }),
-  organization: z.string().min(1, "Please enter your organization's name."),
-  password: z.string().min(8, 'Password must contain at least 8 characters.'),
+  organization: z.string().min(1, "Please enter your organization's name"),
+  password: z.string().min(8, 'Password must contain at least 8 characters'),
 };
--- src/common/utils/httpHandlers.ts
+++ src/common/utils/httpHandlers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { ZodError, ZodSchema } from 'zod';
+import { ZodError, ZodIssue, ZodSchema } from 'zod';
 
 import { ResponseStatus, ServiceResponse } from '@/common/models/serviceResponse';
 
@@ -17,25 +17,31 @@ export const validateRequest = (schema: ZodSchema) => (req: Request, res: Respon
   } catch (err) {
     console.log('Error', err);
     // const errorMessage = concatZodErrorMessages(err as ZodError);
-    const errorMessage = `Invalid input: ${(err as ZodError).errors.map((e) => e.message).join(', ')}`;
+    // const errorMessage = `Invalid input: ${(err as ZodError).errors.map((e) => e.message).join(', ')}`;
+
+    const message = concatZodErrorMessages(err as ZodError);
+    const errorMessage = `Invalid input: ${message}`;
 
     const statusCode = StatusCodes.BAD_REQUEST;
     res.status(statusCode).send(new ServiceResponse<null>(ResponseStatus.Failed, errorMessage, null, statusCode));
   }
 };
 
-// const concatZodErrorMessages = (err: ZodError): string => {
-//   let errorMessage: string = '';
-//
-//   err.errors.forEach((error: ZodIssue) => {
-//     const attributeName = toTitleCase(error.path[0] as string);
-//     errorMessage += `${attributeName} ${error.message}. `;
-//   });
-//   return errorMessage.trim();
-// };
+const concatZodErrorMessages = (err: ZodError): string => {
+  let errorMessage: string = '';
 
-// const toTitleCase = (str: string) => {
-//   return str.replace(/\p{L}+('\p{L}+)?/gu, function (txt) {
-//     return txt.charAt(0).toUpperCase() + txt.slice(1);
-//   });
-// };
+  err.errors.forEach((error: ZodIssue) => {
+    let attributeName = toTitleCase(error.path[0] as string);
+    if (attributeName == 'Body' && error.path.length > 1) {
+      attributeName = toTitleCase(error.path[1] as string);
+    }
+    errorMessage += `${attributeName} ${error.message}. `;
+  });
+  return errorMessage.trim();
+};
+
+const toTitleCase = (str: string) => {
+  return str.replace(/\p{L}+('\p{L}+)?/gu, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.slice(1);
+  });
+};
