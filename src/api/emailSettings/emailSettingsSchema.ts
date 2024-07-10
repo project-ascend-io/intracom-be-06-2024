a96641bf6bb60e2af87cd4ddf2ee1ffd15ec2299
bobbygrdn <bobbygrdn777@gmail.com>
2024-07-10 16:07:14 -0700

    feat: adds a schema for new email settings to be used in post request

--- src/api/emailSettings/emailSettingsSchema.ts
+++ src/api/emailSettings/emailSettingsSchema.ts
@@ -8,7 +8,16 @@ export const ModelID = z.object({
   _id: z.instanceof(ObjectId),
 });
 
+export const NewEmailSettingsSchema = z.object({
+  server: z.string().openapi({ example: 'http://www.localhost.com' }),
+  port: z.number().openapi({ example: 5432 }),
+  username: z.string().openapi({ example: 'root' }),
+  password: z.string().openapi({ example: 'securepassword' }),
+  securityType: z.string().openapi({ example: 'TLS' }),
+});
+
 export const EmailSettingsSchema = z.object({
+  _id: z.string().openapi({ example: '668f0c2ce629e80c6fa7ec7d' }),
   server: z.string().openapi({ example: 'http://www.localhost.com' }),
   port: z.number().openapi({ example: 5432 }),
   username: z.string().openapi({ example: 'root' }),
@@ -25,5 +34,6 @@ export const EmailSettingsTestSchema = z.object({
   email: z.string().openapi({ example: 'email@example.com' }),
 });
 
+export type NewEmailSettings = z.infer<typeof NewEmailSettingsSchema>;
 export type EmailSettings = z.infer<typeof EmailSettingsSchema>;
 export type EmailSettingsTest = z.infer<typeof EmailSettingsTestSchema>;
