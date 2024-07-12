3fd8be016e5eccf239ecb96584700b01b8bf728a
bobbygrdn <bobbygrdn777@gmail.com>
2024-07-11 18:55:29 -0700

    feat: adds organizaiton validation and test email validation schema

--- src/api/emailSettings/emailSettingsValidation.ts
+++ src/api/emailSettings/emailSettingsValidation.ts
@@ -13,7 +13,20 @@ export const PostEmailSettingsSchema = z.object({
     username: z.string().openapi({ example: 'root' }),
     password: z.string().openapi({ example: 'apikey' }),
     securityType: z.string().openapi({ example: 'TLS' }),
+    organization: commonValidations.organization,
   }),
 });
 
+export const PostEmailSettingsTestSchema = z.object({
+  body: z.object({
+    server: z.string().openapi({ example: 'http://www.localhost.com' }),
+    port: z.number().openapi({ example: 5432 }),
+    username: z.string().openapi({ example: 'root' }),
+    password: z.string().openapi({ example: 'apikey' }),
+    securityType: z.string().openapi({ example: 'TLS' }),
+    email: z.string().openapi({ example: 'example@email.com' }),
+  }),
+});
+
+export type PostEmailSettingsTest = z.infer<typeof PostEmailSettingsTestSchema.shape.body>;
 export type PostEmailSettings = z.infer<typeof PostEmailSettingsSchema.shape.body>;
