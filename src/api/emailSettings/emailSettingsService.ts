60a2f2b54c7b17729d9b02e0888a38c8d4b18b3a
bobbygrdn <bobbygrdn777@gmail.com>
2024-07-17 17:51:57 -0700

    feat: updates logic with function that handles both saving and updating email settings

--- src/api/emailSettings/emailSettingsService.ts
+++ src/api/emailSettings/emailSettingsService.ts
@@ -11,14 +11,13 @@ import { ResponseStatus, ServiceResponse } from '@/common/models/serviceResponse
 import { logger } from '@/server';
 
 export const emailSettingsService = {
-  // Retrieves all email settings from the database
   findById: async (id: string): Promise<ServiceResponse<EmailSettings | null>> => {
     try {
       const emailSettings = await emailSettingsRepository.findByIdAsync(id);
       if (!emailSettings) {
         return new ServiceResponse(
           ResponseStatus.Failed,
-          `No Email Settings found with Id ${id}`,
+          `No Email Settings found with organization ${id}`,
           null,
           StatusCodes.NOT_FOUND
         );
@@ -36,55 +35,46 @@ export const emailSettingsService = {
     }
   },
 
-  // Inserts a new email settings into the database
-  insertEmailSettings: async (request: Request): Promise<ServiceResponse<EmailSettings | null>> => {
+  changeEmailSettings: async (request: Request): Promise<ServiceResponse<EmailSettings | null>> => {
     try {
       const emailSettings = NewEmailSettingsSchema.parse({ ...request.body });
-      const newEmailSettings = await emailSettingsRepository.insertEmailSettings(emailSettings);
-
-      return new ServiceResponse<EmailSettings>(
-        ResponseStatus.Success,
-        'Email Settings created.',
-        newEmailSettings,
-        StatusCodes.OK
-      );
-    } catch (err) {
-      const errorMessage = `emailSettingsService - InsertUserProfile - Error Message`;
+      let existingEmailSettings = await emailSettingsRepository.findByIdAsync(emailSettings.organization);
+      if (!existingEmailSettings) {
+        const newEmailSettings = await emailSettingsRepository.insertEmailSettings(emailSettings);
+        return new ServiceResponse<EmailSettings>(
+          ResponseStatus.Success,
+          'Email Settings created.',
+          newEmailSettings,
+          StatusCodes.OK
+        );
+      } else {
+        const updateResult = await emailSettingsRepository.updateEmailSettings(
+          existingEmailSettings.organization,
+          emailSettings
+        );
+        if (updateResult && updateResult.modifiedCount > 0) {
+          existingEmailSettings = await emailSettingsRepository.findByIdAsync(emailSettings.organization);
+          return new ServiceResponse<EmailSettings | null>(
+            ResponseStatus.Success,
+            'Email Settings updated successfully.',
+            existingEmailSettings,
+            StatusCodes.OK
+          );
+        } else {
+          return new ServiceResponse<EmailSettings | null>(
+            ResponseStatus.Failed,
+            'No email settings were updated.',
+            existingEmailSettings,
+            StatusCodes.NOT_FOUND
+          );
+        }
+      }
+    } catch (error) {
+      const errorMessage = `emailSettingsService - ChangeEmailSettings - Error Message`;
       logger.error(errorMessage);
       return new ServiceResponse(ResponseStatus.Failed, errorMessage, null, StatusCodes.INTERNAL_SERVER_ERROR);
     }
   },
-
-  // Changes data on an email settings in the database
-  putEmailSettings: async (id: string, request: Request): Promise<ServiceResponse<null>> => {
-    try {
-      const emailSettings = NewEmailSettingsSchema.parse({ ...request.body });
-
-      const updateResult = await emailSettingsRepository.updateEmailSettings(id, emailSettings);
-
-      if (updateResult && updateResult.modifiedCount > 0) {
-        return new ServiceResponse<null>(
-          ResponseStatus.Success,
-          'Email Settings updated successfully.',
-          null,
-          StatusCodes.OK
-        );
-      } else {
-        return new ServiceResponse<null>(
-          ResponseStatus.Failed,
-          'No email settings was updated.',
-          null,
-          StatusCodes.NOT_FOUND
-        );
-      }
-    } catch (error) {
-      const errorMessage = `emailSettingsService - PutEmailSettings - Error Message`;
-      logger.error(errorMessage);
-      return new ServiceResponse<null>(ResponseStatus.Failed, errorMessage, null, StatusCodes.INTERNAL_SERVER_ERROR);
-    }
-  },
-
-  // Tests the email settings
   testEmailSettings: async (request: Request): Promise<ServiceResponse<null>> => {
     try {
       const emailTestSettings = EmailSettingsTestSchema.parse({ ...request.body });
@@ -94,7 +84,7 @@ export const emailSettingsService = {
       if (testResult) {
         return new ServiceResponse<null>(
           ResponseStatus.Success,
-          'Email Settings tested successfully.',
+          `Test email sent successfully to ${emailTestSettings.email}`,
           null,
           StatusCodes.OK
         );
