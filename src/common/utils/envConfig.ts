b5df63609f73782c78361407555eefdcf0997ff0
Oscar Estrada <16450128+oestrada1001@users.noreply.github.com>
2024-07-28 13:52:46 -0700

    refactor(index, server, and env): add env variables to env template and improve session config

--- src/common/utils/envConfig.ts
+++ src/common/utils/envConfig.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import { cleanEnv, host, num, port, str, testOnly } from 'envalid';
+import { bool, cleanEnv, host, num, port, str, testOnly } from 'envalid';
 
 dotenv.config();
 
@@ -15,4 +15,6 @@ export const env = cleanEnv(process.env, {
   COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
   MONGODB_CONNECTION_STRING: str({ devDefault: testOnly('mongodb://root:root@localhost:27017') }),
   SESSION_SECRET: str({ devDefault: testOnly('M4twM8i3c2wiu0VnchpJDFMk3kS0KSVNESmCY7MnwwMT5aiD3o21NFwv0itexZur') }),
+  SESSION_SECURE: bool({ devDefault: testOnly(false) }),
+  SESSION_HTTP_ONLY: bool({ devDefault: testOnly(false) }),
 });
--- src/common/utils/sessionConfig.ts
+++ src/common/utils/sessionConfig.ts
@@ -1,14 +1,15 @@
 import session from 'express-session';
 
 import { env } from '@/common/utils/envConfig';
+const { SESSION_HTTP_ONLY, SESSION_SECRET, SESSION_SECURE } = env;
 
 export const sessionConfiguration = session({
-  secret: env.SESSION_SECRET.toString(),
+  secret: SESSION_SECRET as string,
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false,
-    httpOnly: false,
+    secure: SESSION_SECURE as boolean,
+    httpOnly: SESSION_HTTP_ONLY as boolean,
     maxAge: 60000 * 60 * 24 * 5, // 5 Days
   },
 });
