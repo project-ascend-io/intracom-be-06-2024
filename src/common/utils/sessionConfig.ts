dd4799affaadf9c7d3d4053f68b8115606637371
Oscar Estrada <16450128+oestrada1001@users.noreply.github.com>
2024-07-28 14:24:24 -0700

    feat(connect-mongo): add mongo store to session config

--- src/common/utils/sessionConfig.ts
+++ src/common/utils/sessionConfig.ts
@@ -1,12 +1,20 @@
+import MongoStore from 'connect-mongo';
 import session from 'express-session';
 
 import { env } from '@/common/utils/envConfig';
-const { SESSION_HTTP_ONLY, SESSION_SECRET, SESSION_SECURE } = env;
+
+const { SESSION_HTTP_ONLY, SESSION_SECRET, SESSION_SECURE, MONGODB_CONNECTION_STRING } = env;
+
+const sessionStore = MongoStore.create({
+  mongoUrl: MONGODB_CONNECTION_STRING as string,
+  collectionName: 'sessions',
+});
 
 export const sessionConfiguration = session({
   secret: SESSION_SECRET as string,
   resave: false,
   saveUninitialized: false,
+  store: sessionStore,
   cookie: {
     secure: SESSION_SECURE as boolean,
     httpOnly: SESSION_HTTP_ONLY as boolean,
