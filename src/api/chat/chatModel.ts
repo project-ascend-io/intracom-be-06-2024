3262511ac3461330911932b4658513581acc8139
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-15 12:12:42 -0700

    feat: creates the mongoose schema for the model

---
+++ src/api/chat/chatModel.ts
@@ -0,0 +1,15 @@
+import mongoose, { Model, Schema } from 'mongoose';
+
+import { Chat } from '@/api/chat/chatSchema';
+
+const mongooseChatSchema = new Schema<Chat>(
+  {
+    chatName: { type: String, required: true },
+    isChannel: { type: Boolean, required: true },
+    users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    chatAdmin: { type: Schema.Types.ObjectId, ref: 'User' },
+  },
+  { timestamps: true }
+);
+
+export const ChatModel: Model<Chat> = mongoose.model<Chat>('Chat', mongooseChatSchema);
