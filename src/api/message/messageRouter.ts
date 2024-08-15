40471977104d1a6ad4ec30d80b457b0eaf008714
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-15 16:46:20 -0700

    feat: creates router to route requests to appropriate service

---
+++ src/api/message/messageRouter.ts
@@ -0,0 +1,51 @@
+import { OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
+import express, { Request, Response, Router } from 'express';
+import { z } from 'zod';
+
+import { MessageSchema } from '@/api/message/messageSchema';
+import { messageService } from '@/api/message/messageService';
+import { GetMessageSchema, PostMessageSchema } from '@/api/message/messageValidation';
+import { createApiResponse, createPostBodyParams } from '@/api-docs/openAPIResponseBuilders';
+import { handleServiceResponse, validateRequest } from '@/common/utils/httpHandlers';
+
+export const messageRegistry = new OpenAPIRegistry();
+
+messageRegistry.register('Message', MessageSchema);
+
+export const messageRouter: Router = (() => {
+  const router = express.Router();
+
+  messageRegistry.registerPath({
+    method: 'get',
+    path: '/messages/{id}',
+    tags: ['Message'],
+    responses: createApiResponse(z.array(MessageSchema), 'Success'),
+    request: {
+      params: GetMessageSchema,
+    },
+  });
+
+  router.get('/:id', validateRequest(GetMessageSchema), async (req: Request, res: Response) => {
+    const serviceResponse = await messageService.getMessages(req);
+
+    handleServiceResponse(serviceResponse, res);
+  });
+
+  messageRegistry.registerPath({
+    method: 'post',
+    path: '/messages',
+    tags: ['Message'],
+    responses: createApiResponse(MessageSchema, 'Success'),
+    request: {
+      body: createPostBodyParams(PostMessageSchema),
+    },
+  });
+
+  router.post('/', validateRequest(PostMessageSchema), async (req: Request, res: Response) => {
+    const serviceResponse = await messageService.postMessage(req);
+
+    handleServiceResponse(serviceResponse, res);
+  });
+
+  return router;
+})();
