5ac17fa11e53aad644c4544ac08ac0f70e9f3869
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-15 16:44:12 -0700

    feat: creates repository to handle requests for data persistence

---
+++ src/api/chat/chatRepository.ts
@@ -0,0 +1,76 @@
+import { ChatModel } from '@/api/chat/chatModel';
+import { Chat } from '@/api/chat/chatSchema';
+import { UserModel } from '@/api/user/userModel';
+
+import { mongoDatabase } from '../mongoDatabase';
+
+export const chatRepository = {
+  startConnection: async () => {
+    const mongoDb = await mongoDatabase.initConnection();
+    return mongoDb;
+  },
+  findAllAsync: async (id: string): Promise<Chat[] | null> => {
+    try {
+      await chatRepository.startConnection();
+      let chats = await ChatModel.find({ users: { $elemMatch: { $eq: id } } })
+        .populate('users', '-password')
+        .populate('chatAdmin', '-password')
+        .sort({ updatedAt: -1 });
+
+      chats = (await UserModel.populate(chats, {
+        path: 'users',
+        select: 'username _id',
+      })) as any;
+
+      return chats;
+    } catch (error) {
+      console.error('Error finding chats with user id: ', error);
+      throw error;
+    }
+  },
+  accessChatAsync: async (recipientId: string, creatorId: string): Promise<Chat> => {
+    try {
+      await chatRepository.startConnection();
+      let isChat = await ChatModel.find({
+        isChannel: false,
+        $and: [{ users: { $elemMatch: { $eq: recipientId } } }, { users: { $elemMatch: { $eq: creatorId } } }],
+      }).populate('users', '-password');
+
+      isChat = (await UserModel.populate(isChat, {
+        path: 'users',
+        select: 'username _id',
+      })) as any;
+
+      if (isChat.length > 0) {
+        return isChat[0];
+      } else {
+        const personToChatWith = await UserModel.findOne({ _id: recipientId });
+
+        const chatData = {
+          chatName: personToChatWith?.username,
+          isChannel: false,
+          users: [recipientId, creatorId],
+        };
+
+        try {
+          const createChat = new ChatModel(chatData);
+          await createChat.save();
+
+          const findChat = await ChatModel.findById(createChat._id);
+
+          if (!findChat) {
+            throw new Error('Chat not found');
+          }
+
+          return findChat;
+        } catch (error) {
+          console.error(error);
+          throw error;
+        }
+      }
+    } catch (error) {
+      console.error('Error accessing chat: ', error);
+      throw error;
+    }
+  },
+};
---
+++ src/api/message/messageRepository.ts
@@ -0,0 +1,42 @@
+import { MessageModel } from '@/api/message/messageModel';
+
+import { mongoDatabase } from '../mongoDatabase';
+import { Message, NewMessage } from './messageSchema';
+
+export const messageRepository = {
+  startConnection: async () => {
+    const mongoDb = await mongoDatabase.initConnection();
+    return mongoDb;
+  },
+
+  findByIdAsync: async (id: string): Promise<Message[] | null> => {
+    try {
+      await messageRepository.startConnection();
+      const messages = await MessageModel.find({ chat: id }).populate('sender', 'username').populate('chat', '_id');
+      return messages;
+    } catch (err) {
+      console.error('Error finding messages with chat id: ', err);
+      throw err;
+    }
+  },
+
+  insertMessageAsync: async (message: NewMessage): Promise<Message> => {
+    try {
+      await messageRepository.startConnection();
+      const newMessage = new MessageModel(message);
+      await newMessage.save();
+      const findMessage = await MessageModel.findById(newMessage._id)
+        .populate('sender', 'username')
+        .populate('chat', '_id');
+
+      if (!findMessage) {
+        throw new Error('Message not found');
+      }
+
+      return findMessage;
+    } catch (err) {
+      console.error('Error inserting message: ', err);
+      throw err;
+    }
+  },
+};
