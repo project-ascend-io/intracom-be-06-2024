2e2ac4147b9e24f1a210f4486744cb3406373aa5
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-16 14:36:26 -0700

    fix: adds default value of null to allow null value

--- src/api/chat/chatModel.ts
+++ src/api/chat/chatModel.ts
@@ -7,7 +7,7 @@ const mongooseChatSchema = new Schema<Chat>(
     chatName: { type: String, required: true },
     isChannel: { type: Boolean, required: true },
     users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    chatAdmin: { type: Schema.Types.ObjectId, ref: 'User' },
+    chatAdmin: { type: Schema.Types.ObjectId, ref: 'User', default: null },
   },
   { timestamps: true }
 );
