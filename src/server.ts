b948b9e9193ee8d004f3c22d3ffe253efb86d354
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-21 21:28:13 -0700

    feat: adds logic to serve the socket docs generated using asyncapi

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express, { Express } from 'express';
 import helmet from 'helmet';
+import path from 'path';
 import { pino } from 'pino';
 
 import { authRouter } from '@/api/auth/authRouter';
@@ -20,6 +21,8 @@ import { sessionConfiguration } from '@/common/utils/sessionConfig';
 
 const logger = pino({ name: 'server start' });
 const app: Express = express();
+const socketDocsPath = path.join(__dirname, './socket-docs');
+console.log(socketDocsPath);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -50,6 +53,9 @@ app.use('/user-invites', userInviteRouter);
 app.use('/organizations', userInviteRouter);
 app.use('/messages', messageRouter);
 app.use('/chats', chatRouter);
+
+// Socket.IO Documentation
+app.use('/socket', express.static(socketDocsPath));
 
 // Swagger UI
 app.use('/api-docs', openAPIRouter);
