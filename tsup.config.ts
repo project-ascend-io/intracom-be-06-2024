23f7451e66637f6fb8fef013b00095810fd3f1ed
bobbygrdn <bobbygrdn777@gmail.com>
2024-08-23 10:16:35 -0700

    feat: adds a flag to ignore the socketdocs dir and uses fsextra to copy that dir into the build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,3 +1,4 @@
+import { copy } from 'fs-extra';
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
@@ -7,5 +8,14 @@ export default defineConfig({
   clean: true,
   loader: {
     '.html': 'text',
+  },
+  ignoreWatch: ['src/socket-docs'],
+  onSuccess: async () => {
+    try {
+      await copy('src/socket-docs', 'dist/socket-docs');
+      console.log('Copied src/socket-docs to dist/socket-docs');
+    } catch (err) {
+      console.error('Error copying socket-docs:', err);
+    }
   },
 });
