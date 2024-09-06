254b0cca86705aaff4709903841bb3c9883fbd47
Oscar Estrada <16450128+oestrada1001@users.noreply.github.com>
2024-09-06 10:32:13 -0700

    fix: defaulting to MemoryStore on express-session only on test

--- src/common/utils/sessionConfig.ts
+++ src/common/utils/sessionConfig.ts
@@ -3,12 +3,20 @@ import session from 'express-session';
 
 import { env } from '@/common/utils/envConfig';
 
-const { SESSION_HTTP_ONLY, SESSION_SECRET, SESSION_SECURE, MONGODB_CONNECTION_STRING } = env;
+const { SESSION_HTTP_ONLY, SESSION_SECRET, SESSION_SECURE, MONGODB_CONNECTION_STRING, NODE_ENV } = env;
 
-const sessionStore = MongoStore.create({
-  mongoUrl: MONGODB_CONNECTION_STRING as string,
-  collectionName: 'sessions',
-});
+/**
+ * Undefined is defaulted to MemoryStore.
+ * MemoryStore is required for CICD Testing on the providers side(i.e. Github Pipelines)
+ */
+let sessionStore = undefined;
+
+if (NODE_ENV !== 'test') {
+  sessionStore = MongoStore.create({
+    mongoUrl: MONGODB_CONNECTION_STRING as string,
+    collectionName: 'sessions',
+  });
+}
 
 export const sessionConfiguration = session({
   secret: SESSION_SECRET as string,
